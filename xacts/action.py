678eb9cf281a15ec040156c3d1ed5682184b7ea1
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-12-05 16:42:09 +0800

    Fix Operation Names

--- xacts/action.py
+++ xacts/action.py
@@ -1,14 +1,16 @@
 class Action:
-    READ_ACTION = "R"
-    WRITE_ACTION = "W"
+    READ_OPERATION = "R"
+    WRITE_OPERATION = "W"
 
     def __init__(self, transaction_info):
         self.info = transaction_info
         self.operation = transaction_info[0]
         assert (
-            self.operation == Action.READ_ACTION
-            or self.operation == Action.WRITE_ACTION
-        ), "For {}: Operation must be {} or {}".format(transaction_info, Action.READ_ACTION, Action.WRITE_ACTION)
+            self.operation == Action.READ_OPERATION
+            or self.operation == Action.WRITE_OPERATION
+        ), "For {}: Operation must be {} or {}".format(
+            transaction_info, Action.READ_OPERATION, Action.WRITE_OPERATION
+        )
 
         self.transaction = transaction_info[1:-1]
         self.object = transaction_info[-1]
@@ -18,8 +20,8 @@ class Action:
             self.object == action.object
             and self.transaction != action.transaction
             and (
-                self.operation == Action.WRITE_ACTION
-                or action.operation == Action.WRITE_ACTION
+                self.operation == Action.WRITE_OPERATION
+                or action.operation == Action.WRITE_OPERATION
             )
         )
 
