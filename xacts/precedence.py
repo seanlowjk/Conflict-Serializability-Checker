faad8326f53fa99707e7eb3776d19191ee045212
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-12-05 15:41:58 +0800

    Fix Abstractions for PrecedenceGraph

--- xacts/precedence.py
+++ xacts/precedence.py
@@ -1,13 +1,19 @@
 class PrecedenceGraph:
     def __init__(self):
         self.graph = {}
+        self.is_acyclic = True 
 
     def add_edge(self, from_xact, to_xact):
-        if from_xact not in self.graph:
-            self.graph[from_xact] = {}
+        from_xact_no = from_xact.transaction
+        to_xact_no = to_xact.transaction
 
-        if to_xact not in self.graph[from_xact]:
-            self.graph[from_xact][to_xact] = True 
+        if from_xact_no not in self.graph:
+            self.graph[from_xact_no] = {}
+
+        if to_xact_no not in self.graph[from_xact_no]:
+            self.graph[from_xact_no][to_xact_no] = True 
     
     def has_conflicting_precedence(self, from_xact, to_xact):
-        return to_xact in self.graph and from_xact in self.graph[to_xact]
+        from_xact_no = from_xact.transaction
+        to_xact_no = to_xact.transactio
+        return to_xact_no in self.graph and from_xact_no in self.graph[to_xact_no]
