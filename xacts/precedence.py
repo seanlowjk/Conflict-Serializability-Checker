c7e7a1edf395c201b9814fef8a966d201d0f0ec2
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2021-12-05 15:56:41 +0800

    Fix Graph Data Structure

--- xacts/precedence.py
+++ xacts/precedence.py
@@ -8,10 +8,10 @@ class PrecedenceGraph:
         to_xact_no = to_xact.transaction
 
         if from_xact_no not in self.graph:
-            self.graph[from_xact_no] = {}
+            self.graph[from_xact_no] = []
 
         if to_xact_no not in self.graph[from_xact_no]:
-            self.graph[from_xact_no][to_xact_no] = True 
+            self.graph[from_xact_no].append(to_xact_no)
     
     def has_conflicting_precedence(self, from_xact, to_xact):
         from_xact_no = from_xact.transaction
@@ -22,7 +22,7 @@ class PrecedenceGraph:
         precedences = []
 
         for from_xact_no, to_xact_nos in self.graph.items():
-            for to_xact_no in to_xact_nos.keys():
+            for to_xact_no in to_xact_nos:
                 precedences.append((from_xact_no, to_xact_no))
 
         return precedences
